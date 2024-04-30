f415b9c65429d03ba4b7b977f1c195ed9ad1bdfb
Roshin C <idroshin2010@gmail.com>
2024-04-29 21:58:34 -0400

    Added some tests for CommonUtils

--- src/test/java/dev/roshin/tools/util/CommonUtilsTest.java
+++ src/test/java/dev/roshin/tools/util/CommonUtilsTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CommonUtilsTest {
 
     @Test
@@ -14,13 +16,14 @@ class CommonUtilsTest {
 
         // Get the file name without extension
         String fileNameWithoutExtension = CommonUtils.getFileNameWithoutExtension(originalPath);
+        assertEquals("example", fileNameWithoutExtension);
 
         // New extension
         String newExtension = "md";
 
         // Create new file path with the same name but a different extension
         Path newPath = originalPath.resolveSibling(fileNameWithoutExtension + "." + newExtension);
-
+        assertEquals("example.md", newPath.getFileName().toString());
         System.out.println("New Path: " + newPath);
     }
 }
